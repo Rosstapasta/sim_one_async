f67b96ae160004d8a898e3e9234c8e55de9fdece
Eric Ross <ericgarth99@gmail.com>
2018-03-06 15:53:14 -0700

    shelf component complete

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -10,7 +10,12 @@ export default class Shelf extends  Component{
         super()
 
         this.state = {
-            shelf: []
+            shelf: [],
+            bin1: [],
+            bin2: [],
+            bin3: [],
+            bin4: [],
+            bin5: []
         }
     }
 
@@ -20,18 +25,46 @@ export default class Shelf extends  Component{
             res => {
                 
                 this.setState({shelf: res.data });
+                console.log(this.state.shelf);
 
-                console.log(this.state.shelf)
+
+                var shelfy = this.state.shelf;
+
+                for(var i in shelfy){
+                    if(shelfy[i].bin === 1){
+                        this.setState({bin1: [shelfy[i]]})
+                        return this.state.bin1;
+                    }else if(shelfy[i].bin === 2){
+                        this.setState({bin2: [shelfy[i]]})
+                        return this.state.bin2;
+
+                    }else if(shelfy[i].bin === 3){
+                        this.setState({bin3: [shelfy[i]]})
+                        return this.state.bin2;
+                    
+                    }else if(shelfy[i].bin === 4){
+                        this.setState({bin4: [shelfy[i]]})
+                        return this.state.bin2;
+                    
+                    }else if(shelfy[i].bin === 5){
+                        this.setState({bin5: [shelfy[i]]})
+                        return this.state.bin2;
+                    
+                    }
+                
+                
+                }
             }
-            
         )
+        
+        
         
     }
 
 
     render(){
         var {shelfletter} = this.props.match.params;
-        var {shelf} = this.state;
+        // var {shelf} = this.state;
 
         return (
             <div>
@@ -49,12 +82,31 @@ export default class Shelf extends  Component{
                 </div>
 
                 <div className="body">
-                   
-                { shelf.length === 2 ?
+                
+
+                    { this.state.bin1[0] ?
                         <div>{<Link to={`/bin/${shelfletter}/1`}><button className="bincontents"></button></Link>}
-                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> }
+                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> } 
+
+                    { this.state.bin2[0] ?
+                        <div>{<Link to={`/bin/${shelfletter}/2`}><button className="bincontents"></button></Link>}
+                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> } 
+
+                    { this.state.bin3[0] ?
+                        <div>{<Link to={`/bin/${shelfletter}/3`}><button className="bincontents"></button></Link>}
+                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> } 
+
+                    { this.state.bin4[0] ?
+                        <div>{<Link to={`/bin/${shelfletter}/4`}><button className="bincontents"></button></Link>}
+                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> } 
+
+                    { this.state.bin5[0] ?
+                        <div>{<Link to={`/bin/${shelfletter}/5`}><button className="bincontents"></button></Link>}
+                        </div> : <Link to='/create'><button className="shelfButt"></button></Link> } 
+
+                        
                    
-                    <Link to={`/bin/${shelfletter}/1`}><button className="bincontents"></button></Link>
+                    {/* <Link to={`/bin/${shelfletter}/1`}><button className="bincontents"></button></Link>
 
                     <Link to={`/bin/${shelfletter}/2`}><button className="bincontents"></button></Link>
 
@@ -62,7 +114,8 @@ export default class Shelf extends  Component{
 
                     <Link to={`/bin/${shelfletter}/4`}><button className="bincontents"></button></Link>
 
-                    <Link to={`/bin/${shelfletter}/5`}><button className="bincontents"></button></Link>
+                    <Link to={`/bin/${shelfletter}/5`}><button className="bincontents"></button></Link> */}
+                    
                     { this.state.shelf[0] ?
                         <div>{this.state.shelf[0].item } {this.state.shelf[0].price } 
                         </div> : null}
